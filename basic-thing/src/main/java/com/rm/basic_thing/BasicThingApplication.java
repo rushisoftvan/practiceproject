da4bd3bb3812f5e00d4b0cd9a4c3a4dc61890642
rushikesh.softvan@gmail.com <rushikesh.softvan@gmail.com>
2025-06-12 13:18:39 +0530

    add the basic thing projec

---
+++ basic-thing/src/main/java/com/rm/basic_thing/BasicThingApplication.java
@@ -0,0 +1,13 @@
+package com.rm.basic_thing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicThingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicThingApplication.class, args);
+	}
+
+}
---
+++ basic-thing/src/test/java/com/rm/basic_thing/BasicThingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm.basic_thing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicThingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
